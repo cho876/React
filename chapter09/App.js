cbcc11fb94472c6768b1f52aca2a25060f22ac13
https://github.com/cho876/Algorithm.git <>
2021-09-04 12:47:35 +0900

    [Chapter 09] map() 함수 사용하기

---
+++ chapter09/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Test from './Test.js';
+import './App.css';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <div>
+      App Component
+      <Test/>
+    </div>
+  );
+}
+
+export default App;
