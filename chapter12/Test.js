83ab3723d2286400dd3178d926b9a9d950642f2a
https://github.com/cho876/Algorithm.git <>
2021-09-06 11:21:08 +0900

    [Chapter 12] state 사용하기

---
+++ chapter12/Test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+class Test extends React.Component{
+    constructor(props){
+        super(props);
+        console.log("in constructor");
+    }
+
+    state={
+        age:0
+    };
+
+    plus=()=>{
+        console.log("plus called");
+        this.setState(stateVal=>({
+            age:stateVal.age + 1
+            })
+        )
+    };
+
+    minus=()=>{
+        console.log("minus called");
+        this.setState(stateVal=>({
+            age:stateVal.age - 1
+            })
+        )
+    };
+
+    render(){
+        return(
+            <div>
+                <input type="button" value="plus" onClick={this.plus}></input>
+                <input type="button" value="minus" onClick={this.minus}></input>
+                <p>My Age is : {this.state.age}</p>
+            </div>
+        );
+    }
+}
+
+export default Test;
